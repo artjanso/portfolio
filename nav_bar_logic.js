32c9f147b21d7f6a70990b4e4eac20b728642e14
Username-ajansons <85576462+Username-ajansons@users.noreply.github.com>
2021-09-18 06:36:32 +0100

    commit - mobile testing

--- nav_bar_logic.js
+++ nav_bar_logic.js
@@ -18,17 +18,26 @@ console.log(isMobile);
 
 // Logic for mobile
 if(isMobile == 'none'){
-  $('#navigation-bar-items').css('display', 'none')
+  $('#navigation-bar').css('display', 'none')
   
   
   function myFunction() {
-    var x = document.getElementById("navigation-bar-items");
+    var x = document.getElementById("navigation-bar");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
   }
+
+  $('#navigation-bar').on('click', () => {
+    if($('#navigation-bar').css('display') == 'none') {
+      $('#navigation-bar').css('display', 'block');
+    } else {
+      $('#navigation-bar').css('display', 'none');
+    }
+  });
+
 }
 
 // Logic for fullscreen
